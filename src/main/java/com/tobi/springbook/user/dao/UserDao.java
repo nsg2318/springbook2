3983935bccd45a45a99e202b6653f6ee72b60c90
sunba <nsg2318@naver.com>
2022-02-04 15:55:52 +0900

    mod : 메서드명 클래스명 용이하게 수정

--- src/main/java/com/tobi/springbook/user/dao/UserDao.java
+++ src/main/java/com/tobi/springbook/user/dao/UserDao.java
@@ -1,31 +1,38 @@
 package com.tobi.springbook.user.dao;
 
 import com.tobi.springbook.user.domain.User;
-import com.tobi.springbook.user.repository.MemoryRepository;
+import com.tobi.springbook.user.repository.Repository;
 
 public class UserDao {
 
   public void add(User user) {
 
     //편의상 연결하는 부분
-    MemoryRepository memoryRepository = getMemoryRepository();
+    Repository repository = getRepository();
 
     //편의상 add 하는 부분
-    memoryRepository.add(user);
+    repository.persist(user);
+
+    //펀의상 연결 끊는 부분
+    System.out.println("closed Repository");
   }
 
   public User get(Long id) {
 
     //편의상 연결하는 부분
-    MemoryRepository memoryRepository = getMemoryRepository();
+    Repository repository = getRepository();
 
     //편의상 get 하는 부분
-    User user = memoryRepository.get(id);
+    User user = repository.export(id);
+
+    //펀의상 연결 끊는 부분
+    System.out.println("closed Repository");
+
     return user;
   }
 
-  private MemoryRepository getMemoryRepository() {
-    return new MemoryRepository();
+  private Repository getRepository() {
+    return new Repository();
   }
 }
 
--- src/main/java/com/tobi/springbook/user/repository/Repository.java
+++ src/main/java/com/tobi/springbook/user/repository/Repository.java
@@ -5,17 +5,17 @@ import com.tobi.springbook.user.domain.User;
 import java.util.HashMap;
 import java.util.Map;
 
-public class MemoryRepository {
+public class Repository {
   private static final Map<Long, User> store = new HashMap<>();
   private static long sequence = 0L;
 
-  public User add(User user) {
+  public User persist(User user) {
     user.setId(++sequence);
     store.put(user.getId(), user);
     return user;
   }
 
-  public User get(Long userId) {
+  public User export(Long userId) {
     User user = store.get(userId);
     return user;
   }
